2dc7970ec7e10b3714e6245f69ff9631e966135e
Arvind Batham <arvind@Arvinds-MacBook-Air.local>
2025-01-11 23:56:20 +0530

    fixing background color prob

--- sm-project/src/App.jsx
+++ sm-project/src/App.jsx
@@ -4,10 +4,10 @@ import Profile from "./components/Profile-project/Profile";
 const App = () => {
   return (
     <>
-      <div className="w-screen h-screen bg-slate-400" >
+      
         {/* <Profile /> */}
         <BackgroundChanger />
-      </div>
+     
     </>
   );
 };
--- sm-project/src/components/BackgroundChanger.jsx
+++ sm-project/src/components/BackgroundChanger.jsx
@@ -1,11 +1,62 @@
-import React from 'react'
+import React, { useState } from "react";
+
+const colors = [
+  {
+    id: 1,
+    color: "red",
+    code: "#ff0000",
+  },
+  {
+    id: 2,
+    color: "yellow",
+    code: "#ffff00",
+  },
+  {
+    id: 3,
+    color: "black",
+    code: "#000000",
+  },
+  {
+    id: 4,
+    color: "purple",
+    code: "#800080",
+  },
+  {
+    id: 5,
+    color: "green",
+    code: "#008000",
+  },
+  {
+    id: 6,
+    color: "blue",
+    code: "#0000ff",
+  },
+  {
+    id: 7,
+    color: "default",
+    code: "#808080", // grey
+  },
+];
 
 const BackgroundChanger = () => {
+  const [color, setColor] = useState("#bb1ccc");
   return (
-    <div>
-      
+    <div className={`w-screen h-screen bg-[${color}] flex flex-col-reverse`}>
+      <div className="flex justify-center h-[60px] w-screen mb-6">
+        <div className="flex justify-between px-6 shadow-md py-3 bg-white w-[95%] rounded ">
+          {colors.map((item) => (
+            <button
+              key={item.id}
+              className="text-blue-800 px-4 py-2 rounded text-center flex items-center justify-center "
+              style={{ backgroundColor: `${item.code}` }}
+            >
+              {item.color}
+            </button>
+          ))}
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default BackgroundChanger
+export default BackgroundChanger;
