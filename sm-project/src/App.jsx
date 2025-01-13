90a83943ddd68cdb2283a1f547d14f79fb548ebe
Arvind Batham <arvind@Arvinds-MacBook-Air.local>
2025-01-13 23:46:48 +0530

    adding routes

--- sm-project/src/App.jsx
+++ sm-project/src/App.jsx
@@ -1,9 +1,11 @@
 import BackgroundChanger from "./components/BackgroundChanger";
 import BirthdayWisher from "./components/BirthdayWisher/BirthdayWisher";
 import GithubProfileCard from "./components/githubProfile/GithubProfileCard";
+import Home from "./components/Home";
 import OtpLogin from "./components/OtpLogin";
 import ParaGenerator from "./components/ParaGenerator";
 import Profile from "./components/Profile-project/Profile";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
@@ -13,7 +15,17 @@ const App = () => {
       {/* <ParaGenerator /> */}
       {/* <GithubProfileCard /> */}
       {/* <OtpLogin /> */}
-      <BirthdayWisher />
+      {/* <BirthdayWisher /> */}
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/background-changer" element={<BackgroundChanger />} />
+        <Route path="/para-generator" element={<ParaGenerator />} />
+        <Route path="/github-profile" element={<GithubProfileCard />} />
+        <Route path="/otp-login" element={<OtpLogin />} />
+        <Route path="/birthday-wisher" element={<BirthdayWisher />} />
+      </Routes>
     </>
   );
 };
